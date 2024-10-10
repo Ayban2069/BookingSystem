8d408557e2f7ccd1ab47e559a3e181f25b9da0ef
Ayban2069 <paoloabella718@gmail.com>
2024-10-11 01:35:22 +0800

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 //test
+let port = document.getElementById("port");
+let destination = document.getElementById("destination");
+let boat = documnet.getElementById("boat");
+let date = document.getElementById("date");
+let btn = document.getElementById("book-btn");
+
+
+btn.onclick = function() {
+    document.getElementById("from").textContent = port;
+    document.getElementById("to").textContent = destination;
+}
